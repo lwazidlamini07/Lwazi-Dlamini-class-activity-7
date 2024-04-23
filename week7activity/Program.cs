ebe564218d3baf8653b902c07c80515bf58a2c51
lwazidlamini07 <167198662+lwazidlamini07@users.noreply.github.com>
2024-04-23 13:56:12 +0200

    Add files via upload

---
+++ week7activity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public interface IPlayable
+{
+    void Play();
+}
+public class Guitar : IPlayable
+{
+    void IPlayable.Play()
+    {
+        Console.WriteLine("GUITAR IS NOW PLAYING!");
+    }
+}
+public class Piano : IPlayable
+{
+    void IPlayable.Play()
+    {
+        Console.WriteLine("PIANO IS NOW PLAYING!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Guitar guitar = new Guitar();
+        Piano piano = new Piano();
+        ((IPlayable)guitar).Play();
+        ((IPlayable)piano).Play();
+    }
+}
